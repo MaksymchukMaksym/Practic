878fc6c7c1814d838005627289be9230c2df3838
MaksymchukMaksym <68642705+MaksymchukMaksym@users.noreply.github.com>
2020-08-31 12:31:59 +0300

    1

---
+++ Practic1/Practic1/Source.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"windows.h"
+using namespace std;
+int main(){
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout <<"Малюнок будинка " << endl;
+	cout << "     / "<< '\\'<<endl;
+	cout << "    /   " << '\\'<<endl;
+	cout << "   /     " << '\\'<<endl;
+	cout << "  /-------" << '\\'<<endl;
+	cout << " / |     | " << '\\'<<endl;
+	cout << "   |     |" << endl;
+	cout << "   |     |" << endl;
+	cout << "   |_____|" << endl;
+
+
+
+return 0;}
---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+class c {
+	int h ;
+public:
+	virtual void Shoot() = 0;
+
+	
+	
+};
+
+
+int main() {
+	
+	
+	return 0;
+}
